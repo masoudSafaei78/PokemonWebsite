035cc564788c90fbc1b7aafdebe205047524e139
Masoud <safaei_masoud@yahoo.com>
2020-12-03 14:57:10 +0330

    app.js is changed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import Header from './Components/Header'
 import LeftSideBar from './Components/LeftSideBar'
 import Home from './Components/Pages/Home'
-import Layout from './Components/Layout';
-import ResponsiveDrawer from '../src/Components/Test'
+// import Layout from './Components/Layout';
+// import ResponsiveDrawer from '../src/Components/Test'
+// import ResponsiveDrawer from '../src/Components/layout'
+import Layout from '../src/Components/Layout'
+// import ResponsiveDrawer from '../src/Components/layout'
 import PokemonCards from '../src/Components/PokemonCards'
 import Test2 from '../src/Components/test2'
+// import ResponsiveDrawer from './Components/Test';
 function App() {
   return (
     <div style={{margin:"50px"}} className="App">
-    {/* <Layout/> */}
-    <ResponsiveDrawer/>
-    {/* <PokemonCards/> */}
-    {/* <Test2/> */}
-    {/* <Home/> */}
+    
+    
+    <Layout/>
+ 
+ 
     </div>
   );
 }
