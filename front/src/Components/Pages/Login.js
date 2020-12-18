e80e85b52d26f39f10b682e46a511f15d4563a2d
Masoud <safaei_masoud@yahoo.com>
2020-12-18 15:16:07 +0330

    change login Styles

--- front/src/Components/Pages/Login.js
+++ front/src/Components/Pages/Login.js
@@ -12,39 +12,11 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import useStyles from '../../Styles/LoginStyle';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+
+
 
 export default function Login() {
   const classes = useStyles();
@@ -110,7 +82,7 @@ export default function Login() {
         </form>
       </div>
       <Box mt={8}>
-        <Copyright />
+       
       </Box>
     </Container>
   );
