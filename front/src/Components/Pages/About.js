f8d143999ff28cde44f783f6056553cb05c70884
Ghazal <ghazal.aghayi@yahoo.com>
2020-12-30 22:51:37 +0330

    "About Pokemon" completed !

--- front/src/Components/Pages/About.js
+++ front/src/Components/Pages/About.js
@@ -3,8 +3,42 @@ import React from 'react'
 function About() {
     return (
         <div>
-            <p>
-                
+            <p style = {{marginTop: "70px" , textAlign : "left" , marginLeft : "70px"}}>
+                <br />
+              <p style = {{fontSize : "33px"}}>
+              About the Pokémon Company International : <br />
+            </p>  
+            <br />
+            <p style = {{fontSize : "22px" , marginRight : "10px"}}>
+            The Pokémon Company International, a subsidiary of The Pokémon Company in Japan, manages the property outside of Asia and is responsible for brand management, licensing, marketing, the Pokémon Trading Card Game, the animated TV series, home entertainment, and the official Pokémon website.
+            Pokémon was launched in Japan in 1996 and today is one of the most popular children's entertainment properties in the world. <br />
+            </p>
+            <p style = {{fontSize : "33px"}}>
+                <br />
+            What Are Pokémon? <br />
+            </p>
+            <p style = {{fontSize : "22px" , marginRight : "20px"}}>
+                <br />
+            Pokémon are creatures of all shapes and sizes who live in the wild or alongside humans. For the most part, Pokémon do not speak except to utter their names.
+            Pokémon are raised and commanded by their owners (called “Trainers”).
+            During their adventures, Pokémon grow and become more experienced and even, on occasion, evolve into stronger Pokémon.
+            There are currently more than 700 creatures that inhabit the Pokémon universe.
+            </p>
+            <p style = {{fontSize : "33px"}}>
+                <br />
+                Pokémon Trading Card Game : 
+                <br />
+            </p>
+            <p style = {{fontSize : "22px" , marginRight : "20px"}}>
+                <br />
+            In the Pokémon Trading Card Game, players build decks around their favorite Pokémon and then play against each other, sending their Pokémon into battle to prove who the best Pokémon Trainer is.
+            Players can begin with theme decks—preconstructed decks designed to cover the basics of the game.
+            Then, they can augment their card collections with booster packs that provide more cards, letting players develop more diverse decks.
+            With thousands of cards to choose from, the game is never the same twice.
+            The Pokémon Company International releases four sets of cards, called “expansions,” each year, so the game continues to evolve and expand for both players and collectors.
+             <br />
+             <br />
+            </p>
             </p>
         </div>
     )
