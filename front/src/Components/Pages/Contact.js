3a4c5fae41d155762ae01bd2e7f90df6854f8067
Ghazal <ghazal.aghayi@yahoo.com>
2020-12-30 22:31:20 +0330

    "Contact Us" completed !

--- front/src/Components/Pages/Contact.js
+++ front/src/Components/Pages/Contact.js
@@ -2,16 +2,24 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div>
+        <div style={{marginTop: "70px"}}>
             <p>
-                Hello ! :))  <br />
+                <p style={{ fontSize: "50px", marginTop: "150px" }}>
+                    Hello ! :))  <br />
+                </p>
+                <p>
+                    <br />
                 We are the Pokemon website producers. "Bowerlas" <br />
                 Ghazal Aghayi, Masoud Safaei, Zahra Khodaverdi, Abbas Tedadi Sangesar <br />
+                    <br />
                 To contact us use the emails below : <br />
                 ghazal.aghayi@yahoo.com <br />
                 masoud_safaei@yahoo.com <br />
                 shayestehkhodaverdi@gmail.com <br />
+                abbas.tedi@gmail.com <br />
+                <br />
                 If you want to see our source code from github, please click on the github botton in the main bar ... <br />
+                </p>
             </p>
         </div>
     )
