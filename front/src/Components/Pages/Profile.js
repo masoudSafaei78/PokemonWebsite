b98b64d87ee7eec830cf91dee196c7a8c70f332a
Ghazal <ghazal.aghayi@yahoo.com>
2021-01-01 15:21:39 +0330

    Profile updated !

--- front/src/Components/Pages/Profile.js
+++ front/src/Components/Pages/Profile.js
@@ -12,19 +12,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Pokemon Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -34,19 +24,41 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    marginTop: "20px",
+    backgroundColor: theme.palette.primary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    // margin: theme.spacing(3, 0, 2),
+    color:theme.palette.secondary.main,
+    // color:theme.palette.secondary.main,
+    width:"200px",
+    fontSize:"20px",
+    background: theme.palette.primary.main,
+    marginTop: "50px"
+  },
+  Profile: {
+    // margin: theme.spacing(3, 0, 2),
+    color:theme.palette.primary.main,
+    // color:theme.palette.secondary.main,
+    width:"150px",
+    fontSize:"30px",
+  },
+  Typography: {
+    marginTop:"20px",
+    color:theme.palette.primary.main,
+    // color:theme.palette.secondary.main,
+    width:"150px",
+    fontSize:"26px"
   },
 }));
 
 export default function Profile() {
+  const Email= (localStorage.getItem('Email')) ;
+  console.log(Email);
   const classes = useStyles();
 
   return (
@@ -54,12 +66,39 @@ export default function Profile() {
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <AccountCircle fontSize="large" />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" className={classes.Profile}>
           Profile
         </Typography>
-        <form className={classes.form} noValidate>
+        <Grid container direction="row" justify="space-around" alignItems="center" style={{marginTop:"20px"}}> 
+        <Grid item >
+        <Typography className={classes.Typography}>
+          Your Email : 
+        </Typography>
+
+        </Grid>
+        <Grid item style={{left:"0px"}} >
+        <Typography className={classes.Typography}>
+          {Email}
+        </Typography>
+
+        </Grid>
+            
+        </Grid>
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick = {()=> localStorage.removeItem('Email')}
+          >
+             Log Out
+          </Button>
+
+       
+        {/* <form className={classes.form} noValidate>
             
           <Button
             type="submit"
@@ -86,11 +125,9 @@ export default function Profile() {
             <Grid item>
             </Grid>
           </Grid>
-        </form>
+        </form> */}
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+    
     </Container>
   );
 }
