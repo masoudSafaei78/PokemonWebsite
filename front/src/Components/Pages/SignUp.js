c4cd558cc5cda3dac0596e043d04601b4b2ac816
Ghazal <ghazal.aghayi@yahoo.com>
2021-01-01 15:33:41 +0330

    SignUp style updated !

--- front/src/Components/Pages/SignUp.js
+++ front/src/Components/Pages/SignUp.js
@@ -12,29 +12,12 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import useStyles from "../../Styles/SignUpStyle";
 
 
 
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: "100px",
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+
 
 export default function SignUp() {
 //   let Data = new FormData();
