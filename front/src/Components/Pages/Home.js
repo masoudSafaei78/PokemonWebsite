ba4274f6e345e0a5d7d4c29779dbb10dc6e8fb29
Ghazal <ghazal.aghayi@yahoo.com>
2021-01-01 23:19:06 +0330

    Home page updated

--- front/src/Components/Pages/Home.js
+++ front/src/Components/Pages/Home.js
@@ -4,20 +4,20 @@ import '../../App.css';
 import Test2 from '../test2';
 import { Grid } from '@material-ui/core';
 function Home() {
-    const [pokemonData,setPokemonData] = useState([])
-   const PokemonData= JSON.parse(localStorage.getItem('PokemonData')) ;
-  
-    return (
-         <Grid style={{background:"#220326",width:"100%",marginLeft:"20px",marginTop:"70px"}}container xs={12} md={12} sm={12} justify="center" alignItems="center" >
+    const [pokemonData, setPokemonData] = useState([])
+    const PokemonData = JSON.parse(localStorage.getItem('PokemonData'));
 
-   
-            {PokemonData.map((item)=>(
+    return (
+        <Grid style={{ background: "#220326", width: "100%", marginLeft: "20px", marginTop: "70px" }} container xs={12} md={12} sm={12} justify="center" alignItems="center" >
+
+
+            {PokemonData.map((item) => (
                 <PokemonCard pid={item.id} pname={item.Name} ptype1={item.Type1} ptype2={item.Type2}
-                ptotal={item.Total} pAttack={item.Attack} pDefense={item.Defense} pSpeed={item.Speed} pLegendary={item.Legendary} />
-                ))}
-             
+                    ptotal={item.Total} pAttack={item.Attack} pDefense={item.Defense} pSpeed={item.Speed} pLegendary={item.Legendary} />
+            ))}
+
         </Grid>
-      
+
     )
 }
 
